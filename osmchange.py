bc3ce489040770cbdfd5e822f68d3baf35303a41
kouki-T <badapple444@gmail.com>
2019-07-02 03:45:50 +0900

    edit osmchange

--- osmchange.py
+++ osmchange.py
@@ -1,8 +1,9 @@
+import requests
 import urllib.request
 import urllib.parse
 import xml.etree.ElementTree as ET
 
-url = "https://www.openstreetmap.org/api/0.6/changeset/20000000"
+url = "https://www.openstreetmap.org/api/0.6/changeset/71779142"
 
 req = urllib.request.Request(url)
 
@@ -12,4 +13,23 @@ root = ET.fromstring(xml_string)
 
 child = root[0]
 
-print(child.tag,child.attrib)
+osm = str(child.attrib)
+osm2 = osm.split()
+
+lat = osm2[17]
+lon = osm2[19]
+
+url2 = "http://www.finds.jp/ws/rgeocode.php?" + "lat=" + lat + "&" + "lon=" + lon
+
+req2 = urllib.request.Request(url2)
+
+with urllib.request.urlopen(req2) as response2:
+    xml_string = response2.read()
+root2 = ET.fromstring(xml_string)
+
+child2 = root2[0]
+
+osm3 = str(child2.tag)
+
+
+print(osm3)
