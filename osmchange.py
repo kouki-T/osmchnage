61f61afad8bb982e937aa776530123a2d8200e9c
Takesue Kouki <badapple444@aoyamagakuinfuruhashikenkyuushitsunoMacBook-ea-2.local>
2019-07-09 09:54:23 +0900

    add json version

--- osmchange.py
+++ osmchange.py
@@ -2,6 +2,7 @@ import requests
 import urllib.request
 import urllib.parse
 import xml.etree.ElementTree as ET
+import json
 
 url = "https://www.openstreetmap.org/api/0.6/changeset/71779142"
 
@@ -16,20 +17,24 @@ child = root[0]
 osm = str(child.attrib)
 osm2 = osm.split()
 
-lat = osm2[17]
-lon = osm2[19]
-
-url2 = "http://www.finds.jp/ws/rgeocode.php?" + "lat=" + lat + "&" + "lon=" + lon
-
-req2 = urllib.request.Request(url2)
-
-with urllib.request.urlopen(req2) as response2:
-    xml_string = response2.read()
-root2 = ET.fromstring(xml_string)
-
-child2 = root2[0]
-
-osm3 = str(child2.tag)
+lat_p = osm2[17].strip("','")
+lon_p = osm2[19].strip("','")
 
 
-print(osm3)
+def rgeocode(lat, lon, res_type):
+    req_url = 'http://www.finds.jp/ws/rgeocode.php?lat={0}&lon={1}'.format(lat,lon)
+    if res_type != None:
+        req_url += '&{0}'.format(res_type)
+        print(req_url)
+
+        return requests.get(req_url)
+
+res = rgeocode(lat_p, lon_p, 'json').text
+
+jsonData = json.loads(res)['result']
+
+pname = jsonData['prefecture']['pname']
+mname = jsonData['municipality']['mname']
+section = jsonData['local'][0]['section']
+
+print("pname={0}, mname={1}, section={2}".format(pname, mname, section))
