f6392be695cf5c27fab2eacad7be66dc136c72c3
Kouki-T <badapple444@gmail.com>
2019-09-28 20:17:07 +0900

    サーチエンジン完成

--- osmchange.py
+++ osmchange.py
@@ -5,12 +5,14 @@ import xml.etree.ElementTree as ET
 import json
 import sqlite3
 
-conn = sqlite3.connect("testsample.db")
+## dbの作成
+conn = sqlite3.connect("testsample1.db")
 cur = conn.cursor()
+cur.execute("create table if not exists geolatlon (lat int, lon int)")
 
+## changesetAPIを叩いて緯度経度を取得後、農研APIで日本かどうか判別。dbへ保存。
 def changesetloop(setnumber):
     url = "https://www.openstreetmap.org/api/0.6/changeset/" + str(setnumber)
-    urlnumber = url.strip("https://www.openstreetmap.org/api/0.6/changeset/")
 
     req = urllib.request.Request(url)
 
@@ -22,25 +24,47 @@ def changesetloop(setnumber):
 
     osm = str(child.attrib)
     osm2 = osm.split()
+    try:
+        lat_p = osm2[17].strip("','")
+        lon_p = osm2[19].strip("','")
+    except IndexError:
+        return print('%s is out of range type.' % setnumber)
 
-    lat_p = osm2[17].strip("','")
-    lon_p = osm2[19].strip("','")
-    
-    if 123 <= float( lon_p) <=140:
-        item = (setnumber , lat_p , lon_p)
-        cur.execute("INSERT INTO geolatlon VALUES (? , ? , ?)" , item)
-        conn.commit()
-        return print('%s is compleated.' % setnumber)
-    else:
-        return print('%s is not Japan.' % setnumber)
-    
+    req_url = "http://www.finds.jp/ws/rgeocode.php?lat={0}&lon={1}&json" .format(lat_p, lon_p)
 
-changesetloop(10000)
-changesetloop(40000)
-changesetloop(60000)
-changesetloop(70127314)
+    res = requests.get(req_url).text
+    jpcheck = json.loads(res)["status"]
 
-cur.execute("SELECT * FROM geolatlon")
-x = cur.fetchall()
-print(x)
+    try:
+        if not jpcheck  == 200:
+            return print('%s is not Japan.' % setnumber)
+        else:
+            item = (lat_p ,lon_p)
+            cur.execute("INSERT INTO geolatlon VALUES (? , ?)" , item)
+            conn.commit()
+            return print('%s is compleated.' % setnumber)
+    except ValueError:
+        return print('%s is unformat xml type.' % setnumber)
+"""
+  try:
+        if 123 <= float(lon_p) <=140:
+            item = (lat_p ,lon_p)
+            cur.execute("INSERT INTO geolatlon VALUES (? , ?)" , item)
+            conn.commit()
+            return print('%s is compleated.' % setnumber)
+        else:
+            return print('%s is not Japan.' % setnumber)
+    except ValueError:
+        return print('%s is unformat xml type.' % setnumber)
+"""
+
+## 検索するchangesetの番号を指定する
+searchnumber = 73775492
+
+## 検索範囲の指定
+for searchchange in range(100):
+    changesetloop(searchnumber)
+    searchnumber +=1
+
+## sqlへのアクセス終了
 conn.close()
